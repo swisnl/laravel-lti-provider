cb7ad8f0918c2c764bc7694b68d9c74fb24fd295
Rolf van de Krol <rvandekrol@swis.nl>
2023-11-22 14:39:54 +0100

    fix: update migrations registration in service provider

--- src/LtiServiceProvider.php
+++ src/LtiServiceProvider.php
@@ -6,15 +6,19 @@ namespace Swis\Laravel\LtiProvider;
 
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Swis\Laravel\LtiProvider\Commands\DeleteExpiredLtiNonces;
 
-class LtiServiceProvider extends \Spatie\LaravelPackageTools\PackageServiceProvider
+class LtiServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
         $package
             ->name('lti-service-provider')
-            ->hasMigration('2023_10_26_100000_add_lti_tables')
+            ->hasMigrations(
+                '2023_10_26_100000_add_client_table',
+                '2023_10_26_200000_add_lti_tables'
+            )
             ->publishesServiceProvider('LtiServiceProvider')
             ->hasConfigFile('lti-provider')
             ->hasCommand(DeleteExpiredLtiNonces::class)
